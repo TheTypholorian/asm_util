d236e753777f5bf750816340ff11e0f972d51bc5
The Typhothanian <thetyphothanian@gmail.com>
2026-08-06 16:42:14 -0600

    removed fabric stuff from dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,9 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
-    maven("https://maven.fabricmc.net")
 }
 
 dependencies {
-    implementation("net.fabricmc:tiny-remapper:0.14.0")
-    implementation("net.fabricmc:mapping-io:0.8.0")
     implementation("org.ow2.asm:asm:9.10.1")
     implementation("org.ow2.asm:asm-tree:9.10.1")
     implementation("org.ow2.asm:asm-util:9.10.1")
